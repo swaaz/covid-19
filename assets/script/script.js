9116d12c5ff10a28d72451b0a11a305161e13774
swaaz <swaasthik.shetty07@gmail.com>
2020-03-28 00:18:42 +0530

    updated

--- assets/script/script.js
+++ assets/script/script.js
@@ -1,54 +1,44 @@
-$.getJSON(`https://coronavirus-19-api.herokuapp.com/all`,function(data) {
-      console.log(data);
-      $(".tcases").append(data.cases);
-      $(".tdeaths").append(data.deaths);
-      $(".trecovered").append(data.recovered);
-      
-    }
-  );
+$.getJSON(`https://coronavirus-19-api.herokuapp.com/all`, function(data) {
+    console.log(data);
+    $(".tcases").append(data.cases);
+    $(".tdeaths").append(data.deaths);
+    $(".trecovered").append(data.recovered);
 
-$.getJSON(`https://coronavirus-19-api.herokuapp.com/countries/india`,function(data) {
-      console.log(data);
-      $(".cases").append(data.cases);
-      $(".active").append(data.active);
-      $(".critical").append(data.critical);
-      $(".deaths").append(data.deaths);
-      $(".recovered").append(data.recovered);
-      $(".todaycases").append(data.todayCases);
-      $(".todaydeaths").append(data.todayDeaths);
-      
-    }
-  );
+});
+
+$.getJSON(`https://coronavirus-19-api.herokuapp.com/countries/india`, function(data) {
+    console.log(data);
+    $(".cases").append(data.cases);
+    $(".active").append(data.active);
+    $(".critical").append(data.critical);
+    $(".deaths").append(data.deaths);
+    $(".recovered").append(data.recovered);
+    $(".todaycases").append(data.todayCases);
+    $(".todaydeaths").append(data.todayDeaths);
+
+});
 
 
 function Country_name() {
     var country = document.getElementById("search-field").value;
     console.log(country);
     $.getJSON(
-      `https://coronavirus-19-api.herokuapp.com/countries/${country}`,
-      function(data) {
-        console.log(data);
-        $(".country").append(country);
-        
-        $(".cases").empty();
-        $(".active").empty();
-        $(".critical").empty();
-        $(".deaths").empty();
-        $(".recovered").empty();
-        $(".todaycases").empty();
-        $(".todaydeaths").empty();
-        $(".country").empty();
-  
-        $(".country").append(data.country);
-        $(".cases").append(data.cases);
-        $(".active").append(data.active);
-        $(".critical").append(data.critical);
-        $(".deaths").append(data.deaths);
-        $(".recovered").append(data.recovered);
-        $(".todaycases").append(data.todayCases);
-        $(".todaydeaths").append(data.todayDeaths);
-        
-      }
+        `https://coronavirus-19-api.herokuapp.com/countries/${country}`,
+        function(data) {
+            console.log(data);
+            $(".country").append(country);
+
+
+
+            $(".country").append(data.country);
+            $(".cases").append(data.cases);
+            $(".active").append(data.active);
+            $(".critical").append(data.critical);
+            $(".deaths").append(data.deaths);
+            $(".recovered").append(data.recovered);
+            $(".todaycases").append(data.todayCases);
+            $(".todaydeaths").append(data.todayDeaths);
+
+        }
     );
-  }
-  
+}
